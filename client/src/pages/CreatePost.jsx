2c4d9ee6622cbc2ba973aa3cd56258de3b39f746
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-07-26 17:33:31 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -27,7 +27,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://dall-e-2-0-6zsn.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -54,7 +54,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-2-0-6zsn.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
