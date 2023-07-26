bcc8cf84030cb31b0ed4b9d2defd7b3e59bd6fae
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-07-26 17:33:55 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
       setloading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-2-0-6zsn.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
