d6ce7116b0b2ba391ff1ad477fd7d8d08bc2138f
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-07-27 12:10:56 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,9 @@ const Home = () => {
       setloading(true);
 
       try {
-        const response = await fetch("https://dall-e-2-0-lu9t.onrender.com/api/v1/post", {
+        // const response = await fetch("https://dall-e-2-0-lu9t.onrender.com/api/v1/post", {
+        const response = await fetch("https://dall-e-clone-00.vercel.app/api/v1/post", {
+      
           method: "GET",
           headers: {
             "Content-Type": "application/json",
